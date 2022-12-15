51b8628a85b35bada57bba1ab08a3429744cfc61
unknown <raghu18767@gmail.com>
2022-12-15 14:09:36 +0530

    add

---
+++ Model/Mail.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelProject.Model
+{
+    public  class Mail
+    {
+
+        public class MailTransferEvent : PubSubEvent<Mail>
+        {
+        }
+        public String MailTo
+        {
+            get;
+            set;
+        }
+        public Attachments Attachments
+        {
+            get;
+            set;
+
+        } 
+        public string MailMessage
+        {
+            get;
+            set;
+        }
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string cc
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ View/Email.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ExcelProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static ExcelProject.Prism.EventSystem;
+
+namespace ExcelProject.View
+{
+    /// <summary>
+    /// Interaction logic for Email.xaml
+    /// </summary>
+    public partial class Email : Window
+    {
+        public Email()
+        {
+            InitializeComponent();
+            this.DataContext = new MailViewModel(Event.EventInstance.EventAggregator);
+        }
+
+      
+    }
+}
